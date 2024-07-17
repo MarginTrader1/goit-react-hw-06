6a0bb2a41eab9193101408b96bf374c80ccfd714
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-07-17 17:44:45 +0300

    стор

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.jsx";
+import { store } from "./redux/store.js";
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "modern-normalize"; // импорт библиотеки модерн нормалайз
 import "./index.css";
@@ -10,7 +12,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
       <BrowserRouter>
-         <App />
+         <Provider store={store}>
+            <App />
+         </Provider>
       </BrowserRouter>
    </React.StrictMode>
 );
