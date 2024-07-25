32162f7915b8ef76ecfc26d805c5cd04e4e6aa07
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-07-25 16:08:44 +0300

    исправил ошибки

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Contact from "../Contact/Contact";
 import { selectContacts } from "../../redux/contactsSlice";
-import { deleteContact } from "../../redux/contactsSlice";
 
 import css from "./ContactList.module.css";
 import { selectNameFilter } from "../../redux/filtersSlice";
 
 const ContactList = () => {
-   const dispatch = useDispatch(); // для отправки экшна в стор 
-
-   //получаем данные из стора 
+   //получаем данные из стора
    const contacts = useSelector(selectContacts);
    const filter = useSelector(selectNameFilter);
 
-   //фильтруем контакты через фильтр 
+   //фильтруем контакты через фильтр
    let filteredContacts = contacts.filter((item) =>
       item.name.toLowerCase().includes(filter.toLowerCase())
    );
@@ -24,10 +21,7 @@ const ContactList = () => {
          {filteredContacts.map(({ id, name, number }) => {
             return (
                <li key={id} className={css.card}>
-                  <Contact name={name} number={number} />
-                  <button className={css.button} onClick={() => dispatch(deleteContact(id))}>
-                     {"Delete"}
-                  </button>
+                  <Contact name={name} number={number} id={id} />
                </li>
             );
          })}
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,12 @@
 import css from "./SearchBox.module.css";
 import { useId } from "react";
-import { changeFilter } from "../../redux/filtersSlice";
-import { useDispatch } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SearchBox = () => {
    const dispatch = useDispatch();
+
+   let filterValue = useSelector(selectNameFilter);
 
    const handleChange = (evt) => {
       dispatch(changeFilter(evt.target.value));
@@ -15,7 +17,7 @@ const SearchBox = () => {
    return (
       <div className={css.input}>
          <label htmlFor={`search-${id}`}>Find contacts by name</label>
-         <input type="text" id={`search-${id}`} onChange={handleChange} />
+         <input type="text" id={`search-${id}`} value={filterValue} onChange={handleChange} />
       </div>
    );
 };
